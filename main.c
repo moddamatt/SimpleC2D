70c722e5e25f26590582d09a988c613497254958
moddamatt <echomatt95@gmail.com>
2021-04-10 00:18:04 -0500

    Simple rotating cube

--- main.c
+++ main.c
@@ -20,6 +20,10 @@
 ********************************************************************************************/
 
 #include "include/raylib.h"
+#include "include/rlgl.h"
+
+void MyCube();
+void PandR(float xPos, float yPos, float zPos, float xAxisAngle, float yAxisAngle, float zAxisAngle);
 
 int main(void)
 {
@@ -29,6 +33,17 @@ int main(void)
     const int screenHeight = 450;
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+    Camera camera = {{0.0f, 10.0f, 10.0f}, {0.0f, 0.0f, 0.0f}, {0.0f, 1.0f, 0.0f}, 45.0f, 0};
+    SetCameraMode(camera, CAMERA_ORBITAL);
+
+    float xPos = 0.0f;
+    float yPos = 0.0f;
+    float zPos = 0.0f;
+
+    float xAxisAngle = 0.0f;
+    float yAxisAngle = 0.0f;
+    float zAxisAngle = 0.0f;
 
     SetTargetFPS(60); // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
@@ -40,14 +55,36 @@ int main(void)
         //----------------------------------------------------------------------------------
         // TODO: Update your variables here
         //----------------------------------------------------------------------------------
+        UpdateCamera(&camera);
 
+        if (xAxisAngle != 360)
+            xAxisAngle += 1.0f;
+        else
+            xAxisAngle = 0.0f;
+
+        if (yAxisAngle != 360)
+            yAxisAngle += 1.0f;
+        else
+            yAxisAngle = 0.0f;
+
+        if (zAxisAngle != 360)
+            zAxisAngle += 1.0f;
+        else
+            zAxisAngle = 0.0f;
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
 
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        BeginMode3D(camera);
+
+        rlPushMatrix();
+        //PandR(xPos, yPos, zPos, xAxisAngle, yAxisAngle, zAxisAngle);
+        MyCube();
+        rlPopMatrix();
+
+        EndMode3D();
 
         EndDrawing();
         //----------------------------------------------------------------------------------
@@ -59,4 +96,50 @@ int main(void)
     //--------------------------------------------------------------------------------------
 
     return 0;
+}
+
+void MyCube()
+{
+
+    rlBegin(RL_QUADS);
+    rlColor3f(0.0f, 1.0f, 0.0f);
+    rlVertex3f(1.0f, 1.0f, -1.0f);
+    rlVertex3f(-1.0f, 1.0f, -1.0f);
+    rlVertex3f(-1.0f, 1.0f, 1.0f);
+    rlVertex3f(1.0f, 1.0f, 1.0f);
+
+    rlVertex3f(1.0f, -1.0f, 1.0f);
+    rlVertex3f(-1.0f, -1.0f, 1.0f);
+    rlVertex3f(-1.0f, -1.0f, -1.0f);
+    rlVertex3f(1.0f, -1.0f, -1.0f);
+
+    rlVertex3f(1.0f, 1.0f, 1.0f);
+    rlVertex3f(-1.0f, 1.0f, 1.0f);
+    rlVertex3f(-1.0f, -1.0f, 1.0f);
+    rlVertex3f(1.0f, -1.0f, 1.0f);
+
+    rlVertex3f(1.0f, -1.0f, -1.0f);
+    rlVertex3f(-1.0f, -1.0f, -1.0f);
+    rlVertex3f(-1.0f, 1.0f, -1.0f);
+    rlVertex3f(1.0f, 1.0f, -1.0f);
+
+    rlVertex3f(-1.0f, 1.0f, 1.0f);
+    rlVertex3f(-1.0f, 1.0f, -1.0f);
+    rlVertex3f(-1.0f, -1.0f, -1.0f);
+    rlVertex3f(-1.0f, -1.0f, 1.0f);
+
+    rlVertex3f(1.0f, 1.0f, -1.0f);
+    rlVertex3f(1.0f, 1.0f, 1.0f);
+    rlVertex3f(1.0f, -1.0f, 1.0f);
+    rlVertex3f(1.0f, -1.0f, -1.0f);
+    rlEnd();
+}
+
+void PandR(float xPos, float yPos, float zPos, float xAxisAngle, float yAxisAngle, float zAxisAngle)
+{
+    //TODO: ROTATE AND TRANSLATE
+    rlTranslatef(xPos, yPos, zPos);
+    rlRotatef(xAxisAngle, 1.0f, 0.0f, 0.0f);
+    rlRotatef(yAxisAngle, 0.0f, 1.0f, 0.0f);
+    rlRotatef(zAxisAngle, 0.0f, 0.0f, 1.0f);
 }
